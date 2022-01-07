ce1a748405f46d2be32f2866d2c6732823e92e2b
Lorttexwolf <comisgammer@gmail.com>
2022-01-06 20:22:33 -0600

    Added GET user/ Ratelimit test
    The rate limit is 60 req per 60 seconds, I have it set to 80 requests to make sure we will hit the limit.

--- src/Bloxlink.Net.Tests/UserEndpointTest.cs
+++ src/Bloxlink.Net.Tests/UserEndpointTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bloxlink;
 using System.Threading.Tasks;
+using System;
 
 namespace Bloxlink.Tests
 {
@@ -23,6 +24,24 @@ namespace Bloxlink.Tests
             var user = await client.GetUser(474413050594721802, 372036754078826496);
 
             Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public async Task GetUserWithRatelimit()
+        {
+            using var client = new BloxlinkClient();
+
+            // Exceed rate limits to test RetryOnRatelimit
+            for (var i = 0; i < 80; i++)
+            {
+                // Do not use the cache during this test.
+                var user = await client.GetUser(84117866944663552, false, options: new Rest.BloxlinkRestRequestOptions()
+                {
+                    RetryOnRatelimit = true
+                });
+
+                Assert.IsNotNull(user);
+            }
         }
     }
 }
